bf504852ec157022ebc2c2c66441d909e72abfc4
hunter <87490244+hunterhacker29@users.noreply.github.com>
2024-09-27 18:09:17 +0530

    Update Firebase.jsx

--- src/firebase/Firebase.jsx
+++ src/firebase/Firebase.jsx
@@ -5,13 +5,13 @@ import { getStorage } from 'firebase/storage'; // Import Firebase Storage
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-MRw6GwlwhSMnozahxTH-CmiGHCcmmyQ",
-  authDomain: "movieweb-f2893.firebaseapp.com",
-  projectId: "movieweb-f2893",
-  storageBucket: "movieweb-f2893.appspot.com",
-  messagingSenderId: "308154575917",
-  appId: "1:308154575917:web:618e37cf9587ccc41ddca4",
-  measurementId: "G-Z3JCSHZ1JD"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
